bec095f18bc64a64d1a4275a79e29de4dcd6c378
Riya <riya@Riyas-MacBook-Air.local>
2026-05-25 19:20:43 +0545

    fix: Use correct TrimTime_logo.png and remove empty SVG
    - Fixed navbar to use TrimTime_logo.png (original barber head logo)
    - Removed empty BarberTime_logo.svg file
    - Logo now displays correctly in navbar
    - Professional barber head icon visible

--- resources/views/Frontend/Partials/navbar.blade.php
+++ resources/views/Frontend/Partials/navbar.blade.php
@@ -3,7 +3,7 @@
 
         <!-- brand -->
         <a class="navbar-brand trimtime-brand" href="{{ route('home') }}">
-            <img src="{{ asset('images/BarberTime_logo.png') }}" class="trimtime-brand-logo" alt="BarberTime">
+            <img src="{{ asset('images/TrimTime_logo.png') }}" class="trimtime-brand-logo" alt="BarberTime">
         </a>
 
         <button class="navbar-toggler border-0 shadow-none" type="button"
